e49018b07119e24cfcafc1604beac90fcf31257a
huchenlei <chenlei.hu@mail.utoronto.ca>
2019-03-10 19:28:34 -0400

    :fire: remove unnecessary files

--- spotbugs/src/test/java/ca/utoronto/ece496/utils/DebugTest.java
+++ spotbugs/src/test/java/ca/utoronto/ece496/utils/DebugTest.java
@@ -1,34 +1,9 @@
-//package ca.utoronto.ece496.utils;
-//
-//import edu.umd.cs.findbugs.FindBugs2;
-//import org.junit.Test;
-//
-//import java.io.File;
-//
-///**
-// * This test file is used for debugging purpose
-// * <p>
-// * Created by Charlie on 19. 01 2019
-// */
-//public class DebugTest {
-//    private static final String BASE_DIR = "/Users/Charlie/repos/ECE419/";
-//
-//    @Test
-//    public void testRunFromCmdLine() throws Exception {
-//        FindBugs2.main(new String[]{
-//                "-sourcepath", BASE_DIR + String.join(File.separator, "src", "main", "java"),
-//                "-auxclasspath", BASE_DIR + "libs" + File.separator,
-//                BASE_DIR + "ecs.jar"
-//        });
-//    }
-//}
-
 package ca.utoronto.ece496.utils;
 
-        import edu.umd.cs.findbugs.FindBugs2;
-        import org.junit.Test;
+import edu.umd.cs.findbugs.FindBugs2;
+import org.junit.Test;
 
-        import java.io.File;
+import java.io.File;
 
 /**
  * This test file is used for debugging purpose
