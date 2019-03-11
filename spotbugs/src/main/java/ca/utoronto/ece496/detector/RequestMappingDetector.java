75ef79b8f23c0288eead71bda23125f91b948c5e
huchenlei <chenlei.hu@mail.utoronto.ca>
2019-03-10 22:24:25 -0400

    :construction: report bug to cmdline

--- spotbugs/src/main/java/ca/utoronto/ece496/detector/RequestMappingDetector.java
+++ spotbugs/src/main/java/ca/utoronto/ece496/detector/RequestMappingDetector.java
@@ -3,21 +3,25 @@ package ca.utoronto.ece496.detector;
 import ca.utoronto.ece496.spring.SpringAppEntryPointCreator;
 import ca.utoronto.ece496.utils.GeneralUtil;
 import ca.utoronto.ece496.utils.SootFormatAdapter;
-import edu.umd.cs.findbugs.BugReporter;
-import edu.umd.cs.findbugs.Detector2;
-import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.*;
 import edu.umd.cs.findbugs.ba.AnalysisContext;
 import edu.umd.cs.findbugs.ba.XMethod;
 import edu.umd.cs.findbugs.classfile.CheckedAnalysisException;
 import edu.umd.cs.findbugs.classfile.ClassDescriptor;
+import edu.umd.cs.findbugs.classfile.MethodDescriptor;
 import edu.umd.cs.findbugs.classfile.analysis.AnnotationValue;
+import soot.jimple.Stmt;
 import soot.jimple.infoflow.Infoflow;
 import soot.jimple.infoflow.InfoflowConfiguration;
+import soot.jimple.infoflow.results.DataFlowResult;
 import soot.jimple.infoflow.results.InfoflowResults;
+import soot.jimple.infoflow.results.ResultSinkInfo;
+import soot.jimple.infoflow.results.ResultSourceInfo;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,7 +36,9 @@ public class RequestMappingDetector implements Detector2 {
      */
     private static List<String> entryPoints = new ArrayList<>();
 
-    private BugReporter bugReporter;
+    private final BugReporter bugReporter;
+
+    private final BugAccumulator accumulator;
 
     /**
      * Note: every detector type must have an constructor accepting a bugReporter
@@ -44,6 +50,7 @@ public class RequestMappingDetector implements Detector2 {
      */
     public RequestMappingDetector(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
+        this.accumulator = new BugAccumulator(bugReporter);
     }
 
     /**
@@ -93,6 +100,7 @@ public class RequestMappingDetector implements Detector2 {
         InfoflowConfiguration.PathConfiguration pathConfiguration = config.getPathConfiguration();
         pathConfiguration.setPathReconstructionMode(InfoflowConfiguration.PathReconstructionMode.Precise);
 
+        // Note the content of source/sink should come from user config
         List<String> sources = new ArrayList<>(
                 Arrays.asList("<java.io.BufferedReader: java.lang.String readLine()>")
         );
@@ -119,6 +127,33 @@ public class RequestMappingDetector implements Detector2 {
     }
 
     private void reportBugs(InfoflowResults results) {
-        results.printResults();
+        for (DataFlowResult dataFlowResult : results.getResultSet()) {
+            ResultSinkInfo sink = dataFlowResult.getSink();
+            ResultSourceInfo source = dataFlowResult.getSource();
+
+            Stmt[] path = source.getPath();
+            LinkedList<Stmt> fullPath =
+                    path != null ? new LinkedList<>(Arrays.asList(path)) : new LinkedList<>();
+
+            fullPath.addFirst(source.getStmt());
+            fullPath.addLast(sink.getStmt());
+
+            for (Stmt stmt : fullPath) {
+                if (stmt.containsInvokeExpr()) {
+                    String sootMethodSignature = stmt.getInvokeExpr().getMethod().toString();
+                    MethodDescriptor descriptor = SootFormatAdapter.toMethodDescriptor(sootMethodSignature);
+
+                    accumulator.accumulateBug(
+                            new BugInstance(this, "GENERAL_XSS", Priorities.NORMAL_PRIORITY)
+                                    .addClassAndMethod(descriptor),
+                            SourceLineAnnotation.forFirstLineOfMethod(descriptor)
+                            );
+                } else {
+                    bugReporter.logError("Statement does not contain invoke expr " + stmt);
+                }
+            }
+
+            accumulator.reportAccumulatedBugs();
+        }
     }
 }
